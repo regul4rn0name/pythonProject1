d309fe544eb19cc4f39482d30feae5333156301d
regul4rn0name <komlyk07@gmail.com>
2023-10-24 20:57:17 +0200

    aa

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ bot = telebot.TeleBot("6877931851:AAHIpMInvzR0dTDXhy2PCeucEFojw0PFc9c", parse_mo
 def handle_all_messages(message):
     # Process the received message
     if message.text.lower() == "да":
-        bot.reply_to(message, "Пизда")
+        image_url = "https://i.imgur.com/vQSkmHE.jpg"
+        bot.send_photo(message.chat.id, image_url)
     elif message.text.lower() == "нет":
         bot.reply_to(message, "Пидора ответ")
 
